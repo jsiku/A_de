e5e23a6116e770dfe1647785d5995d2a244255cb
jsiku <j.siku.cho@gmail.com>
2026-03-13 21:51:51 +0100

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import pagefind from "astro-pagefind"; // ✨ 1. 플러그인 불러오기
 import cloudflare from "@astrojs/cloudflare"; // ✨ 어댑터 불러오기
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
@@ -10,7 +12,7 @@ export default defineConfig({
     imageService: "compile", // ✨ Cloudflare 이미지 처리 에러 방지
   }),
   // ✨ 2. integrations 배열 안에 pagefind()를 추가합니다.
-  integrations: [pagefind()],
+  integrations: [pagefind(), sitemap()],
   vite: {
     ssr: {
       external: ["node:path/posix"],
